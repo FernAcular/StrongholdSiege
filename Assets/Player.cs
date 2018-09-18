33bb611406a865a01187b87f54e49d44f60ce389
Salaheldin Akl <salaheldin.akl@student.dit.ie>
2018-09-18 23:48:55 +0100

    Add simple player objects (#22)

---
+++ Assets/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string player_name;
+    public Color color;
+}
