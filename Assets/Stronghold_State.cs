3cf1ae997b87c3156da9e1e82f91f09d6434dd52
QUzair <40413240+QUzair@users.noreply.github.com>
2018-09-22 17:57:36 +0100

    Adding Strongholds (#24)
    * Fixed Issue 14
    
    * Fix Issue 14
    
    * Fixing Issue 14
    
    * Fixed Comments for #14
    
    * Scene Update
    
    * Deletion of Stronghold 6

---
+++ Assets/Stronghold_State.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Stronghold_State : MonoBehaviour {
+
+    //Player color and name Properties
+    public Player player;
+
+    //Stronghold magnitude
+    public int magnitude = 0;
+
+    //Rendering Component contains Material Color
+    public Renderer rend;
+
+    void Start() {
+
+        //Get Rendering Component
+        rend = GetComponent<Renderer>();
+
+        //Instantiate Color and Magnitude
+        if (player != null)
+        {
+            Debug.Log("Changing Color");
+            rend.material.color = player.color;
+        }
+        else
+        {
+            Debug.Log("Player doesnt exist");
+        }
+
+    }
+
+}
