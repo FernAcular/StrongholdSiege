dc101fc6bd88aa24834acb86023b29e4e3394741
Uzair Qureshi <40413240+QUzair@users.noreply.github.com>
2018-10-30 20:01:44 +0000

    Structure Changes (#58)

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public string playerName;
+    public Color color;
+}
--- Assets/TargetClick.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public class TargetClick : MonoBehaviour {
-    
-    void OnMouseDown() {
-        FindObjectOfType<GameEngine>().RegisterStrongholdClick(transform);
-    }
-}
